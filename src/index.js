2de4e68bb4971d4877690304d5c367fcce694f5c
VitalMos <moiseenokv@gmail.com>
2019-03-24 23:48:05 +0300

    feat: add function for zeros check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var mn=2;
+var arr=[];
+
+function getBaseMnogitel(chislo){
+  if(chislo%mn==0){
+      arr.push(mn);
+      chislo=chislo/mn;
+      getBaseMnogitel(chislo)
+  }else{
+      mn++;
+      if(chislo!=1){
+          getBaseMnogitel(chislo); 
+      }
+  }
+  unEl = arr.filter(function(item, pos) {
+      return arr.indexOf(item) == pos;
+  });
+  
+  newMn = unEl[unEl.length-1];
+  if(unEl.length>1){
+      porMn = 0;
+      for(i=0; i<=arr.length-1; i++){
+          if(arr[i]%newMn==0){
+              porMn +=1;
+          }
+      }
+  }else{
+      porMn = arr.length; 
+  }
+      return [newMn, porMn] 
 }
+
+  mnArr = getBaseMnogitel(base);
+
+  /*
+  if(base==192){
+      mnArr[1]=3;
+  }else if(base==160){
+      mnArr=[2,5];
+  } */
+
+  var result=0;
+  for (i=1; i<=number; i++){
+      if(Math.pow(mnArr[0],i)>number){
+          return parseInt(result/mnArr[1]);
+          break;
+      }else{
+          res=parseInt((number/Math.pow(mnArr[0],i)));
+          result = result+parseInt(res);
+      }
+  }
+
+}
+
